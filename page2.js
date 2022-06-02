00f0e64324404281df263cb443b73818540c827d
yadhuvarshini <yadhuvarshini1@gmail.com>
2022-06-02 12:09:55 +0530

    animation

---
+++ page2.js
@@ -0,0 +1,94 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+// Torus
+
+
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
+// Background
+
+
+
+// Avatar
+
+const jeffTexture = new THREE.TextureLoader().load('download.jpg');
+
+const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+
+scene.add(jeff);
+
+// Moon
+
+
+jeff.position.z = -5;
+jeff.position.x = 0;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  
+
+  jeff.rotation.y += 0.01;
+  jeff.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  jeff.rotation.x += 0.01;
+  jeff.rotation.y += 0.006;
+  jeff.rotation.z += 0.01;
+
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
